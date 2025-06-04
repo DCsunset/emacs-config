2d7258bdc56d6f882436d72b51713845193a91e5
DCsunset <DCsunset@protonmail.com>
2025-06-04 14:17:01 +0000

    ai: allow customizing models

--- config/ai.el
+++ config/ai.el
@@ -5,24 +5,18 @@
   (let ((coding-model (make-llm-ollama
                        :host "@OLLAMA_HOST@"
                        :port 80
-		                   :chat-model "qwen2.5-coder:3b-instruct-q4_K_M"
+		                   :chat-model "@OLLAMA_CODING_MODEL@"
 		                   :embedding-model "nomic-embed-text"
                        :default-chat-non-standard-params '(("num_ctx" . 32768))))
         (chat-model (make-llm-ollama
                      :host "@OLLAMA_HOST@"
                      :port 80
-		                 :chat-model "qwen2.5:3b-instruct-q4_K_M"
+		                 :chat-model "@OLLAMA_CHAT_MODEL@"
 		                 :embedding-model "nomic-embed-text"
-                     :default-chat-non-standard-params '(("num_ctx" . 32768))))
-        (large-chat-model (make-llm-ollama
-                           :host "@OLLAMA_HOST@"
-                           :port 80
-		                       :chat-model "deepseek-r1:7b"
-		                       :embedding-model "nomic-embed-text"
-                           :default-chat-non-standard-params '(("num_ctx" . 32768)))))
+                     :default-chat-non-standard-params '(("num_ctx" . 32768)))))
     (setopt ellama-provider chat-model)
     (setopt ellama-coding-provider coding-model)
     (setopt ellama-naming-provider chat-model)
-    (setopt ellama-translation-provider large-chat-model)
-    (setopt ellama-summarization-provider large-chat-model)))
+    (setopt ellama-translation-provider chat-model)
+    (setopt ellama-summarization-provider chat-model)))
 
